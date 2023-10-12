40f2d349301738c49927d1d458eba2fd14fc344d
jamesh21 <jamesh21@uw.edu>
2023-10-12 00:17:34 -0700

    added side nav menu

--- index.js
+++ index.js
@@ -8,4 +8,20 @@ function setArticleImages() {
     });
 }
 
-setArticleImages()
+function addButtonEventListeners() {
+    const menuBtn = document.querySelector('.hamburger-icon');
+    const closeMenuBtn = document.querySelector('.close-icon');
+    const navMenu = document.querySelector('.off-screen-menu');
+    const overlay = document.querySelector('.overlay');
+    menuBtn.addEventListener('click', () => {
+        navMenu.classList.toggle('is-active');
+        overlay.style.display = 'block';
+    });
+
+    closeMenuBtn.addEventListener('click', () => {
+        navMenu.classList.toggle('is-active');
+        overlay.style.display = 'none';
+    });
+}
+setArticleImages();
+addButtonEventListeners();
