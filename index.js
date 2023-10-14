432bf894dbfa793297d82361743bddeeaa1f72b9
jamesh21 <jamesh21@uw.edu>
2023-10-14 15:29:20 -0700

    moved unnecessary styles

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Set article with associated background image
-function setArticleImages() {
+const setArticleImages = () => {
     const prefix = 'assets/images/';
     const images = ['image-retro-pcs.jpg', 'image-top-laptops.jpg', 'image-gaming-growth.jpg'];
     const articles = document.querySelectorAll('.article-image');
@@ -8,20 +8,24 @@ function setArticleImages() {
     });
 }
 
-function addButtonEventListeners() {
+// add event listners to hamburger and close icon
+const addButtonEventListeners = () => {
     const menuBtn = document.querySelector('.hamburger-icon');
     const closeMenuBtn = document.querySelector('.close-icon');
     const navMenu = document.querySelector('.off-screen-menu');
     const overlay = document.querySelector('.overlay');
     menuBtn.addEventListener('click', () => {
+        // slides out nav menu when clicked
         navMenu.classList.toggle('is-active');
         overlay.style.display = 'block';
     });
 
     closeMenuBtn.addEventListener('click', () => {
+        // closes nav menu when clicked
         navMenu.classList.toggle('is-active');
         overlay.style.display = 'none';
     });
 }
+
 setArticleImages();
 addButtonEventListeners();
